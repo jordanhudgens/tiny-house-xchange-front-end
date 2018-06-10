4911228e914fc95c7cda9989190a04bbd669ee49
jordanhudgens <jordan@devcamp.com>
2018-06-09 18:35:50 -0600

    Implemented initial registration component

---
+++ src/components/Registration.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Register</h2>
+
+    <form @submit.prevent="submitRegistration"></form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Registration',
+  data() {
+    return {
+      email: null,
+      password: null,
+      password_confirmation: null
+    }
+  },
+  methods: {
+    submitRegistration() {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import About from "./views/About.vue";
+import Registration from "./components/Registration.vue";
 
 Vue.use(Router);
 
@@ -17,6 +18,11 @@ export default new Router({
       path: "/about",
       name: "about",
       component: About
+    },
+    {
+      path: "/register",
+      name: "Registration",
+      component: Registration
     }
   ]
 });
