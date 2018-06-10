ff798bbfabced519f4f7bfd9e8412ff90222984d
jordanhudgens <jordan@devcamp.com>
2018-06-09 18:50:39 -0600

    Added registration axios call

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -2,11 +2,27 @@
   <div>
     <h2>Register</h2>
 
-    <form @submit.prevent="submitRegistration"></form>
+    <form @submit.prevent="submitRegistration">
+      <div>
+        <input type="text" v-model="email">
+      </div>
+
+      <div>
+        <input type="password" v-model="password">
+      </div>
+
+      <div>
+        <input type="password" v-model="password_confirmation">
+      </div>
+
+      <button type="submit">Register</button>
+    </form>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Registration',
   data() {
@@ -18,7 +34,23 @@ export default {
   },
   methods: {
     submitRegistration() {
+      axios
+        .post("https://tiny-house-exchange-service.herokuapp.com/register_user",
+        {
+          user: {
+            email: this.email,
+            password: this.password,
+            password_confirmation: this.password_confirmation
+          }
+        })
+        .then(response => {
+          console.log('created successfully', response.data);
 
+          return response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        })
     }
   }
 }
