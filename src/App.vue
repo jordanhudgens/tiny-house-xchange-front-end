a763e2cf6fbd4c5136faea06ad80094f363714f4
jordanhudgens <jordan@devcamp.com>
2018-07-12 15:18:47 -0600

    Added seller dashboard link

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/seller-dashboard">Dashboard</router-link>
     </div>
     <router-view/>
   </div>
@@ -16,6 +17,7 @@
   text-align: center;
   color: #2c3e50;
 }
+
 #nav {
   padding: 30px;
   a {
