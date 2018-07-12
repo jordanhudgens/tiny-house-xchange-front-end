f7ff3002f9efc3ee578fb03d0a9fdd3230ced483
jordanhudgens <jordan@devcamp.com>
2018-07-12 15:28:21 -0600

    Created the home form component and called it from the seller dashboard

---
+++ src/components/HomeForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Home form
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'HomeForm',
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/SellerDashboard.vue
+++ src/views/SellerDashboard.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
     Seller dashboard
+
+    <div>
+      <HomeForm />
+    </div>
   </div>
 </template>
 
 <script>
+import HomeForm from '@/components/HomeForm';
+import axios from 'axios';
+
 export default {
   name: 'SellerDashboard',
+  components: {
+    HomeForm
+  },
   data() {
     return {
     }
