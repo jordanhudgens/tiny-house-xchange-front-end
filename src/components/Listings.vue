0cfb694fce3fd478f64a21e02331f11d2a1c3bb6
jordanhudgens <jordan@devcamp.com>
2018-09-15 23:37:24 -0700

    Added initial home query component

--- src/components/Listings.vue
+++ src/components/Listings.vue
@@ -5,10 +5,29 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Listings',
   data() {
     return {
+    }
+  },
+  beforeMount() {
+    this.getListings();
+  },
+  methods: {
+    getListings() {
+      axios
+        .get("https://tiny-house-exchange-service.herokuapp.com/homes")
+        .then(response => {
+          console.log('responseeeee', response.data);
+
+          return response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        })
     }
   }
 }
