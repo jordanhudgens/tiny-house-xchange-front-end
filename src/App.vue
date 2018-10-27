4c807205b2460ce886cb77c1d3b4ef04bc9276ce
jordanhudgens <jordan@devcamp.com>
2018-10-27 16:11:23 -0500

    Integrated font awesome and additional nav styles

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,37 @@
   <div id="app">
     <div class="nav">
       <div class="nav-links">
-        <router-link class="nav-link" to="/">Home</router-link>
-        <router-link class="nav-link" to="/about">About</router-link>
+        <router-link class="nav-link" to="/">
+          <i class="fas fa-list-ol"></i>
+          <span class="link-text">
+            Recent
+          </span>
+        </router-link>
+
+        <router-link class="nav-link" to="/">
+          <i class="fas fa-chart-line"></i>
+          <span class="link-text">
+            Popular
+          </span>
+        </router-link>
+
         <router-link class="nav-link" to="/">
           <img class="nav-logo" src="https://s3.amazonaws.com/interweave-networks-static/tinyhouse.png">
         </router-link>
-        <router-link class="nav-link" to="/seller-dashboard">Dashboard</router-link>
-        <router-link class="nav-link" to="/seller-dashboard">Another Link</router-link>
+
+        <router-link class="nav-link" to="/">
+          <i class="fas fa-random"></i>
+          <span class="link-text">
+            Random
+          </span>
+        </router-link>
+
+        <router-link class="nav-link" to="/">
+          <i class="far fa-plus-square"></i>
+          <span class="link-text">
+            Submit
+          </span>
+        </router-link>
       </div>
     </div>
 
