73dcba9d9e38977ccf99e72b374ebc31625f7cae
jordanhudgens <jordan@devcamp.com>
2018-10-27 16:05:03 -0500

    Integrated the logo and finished nav styles

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
       <div class="nav-links">
         <router-link class="nav-link" to="/">Home</router-link>
         <router-link class="nav-link" to="/about">About</router-link>
-        <router-link class="nav-link" to="/about">TODO: LOGO</router-link>
+        <router-link class="nav-link" to="/">
+          <img class="nav-logo" src="https://s3.amazonaws.com/interweave-networks-static/tinyhouse.png">
+        </router-link>
         <router-link class="nav-link" to="/seller-dashboard">Dashboard</router-link>
         <router-link class="nav-link" to="/seller-dashboard">Another Link</router-link>
       </div>
